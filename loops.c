b4c91850b368b4e3bfc17aac14c9763da33d29d1
Abinandhu-P <159160073+Abinandhu-P@users.noreply.github.com>
2025-02-12 21:46:20 +0530

    Add files via upload

---
+++ loops.c
@@ -0,0 +1,67 @@
+#include<stdio.h>
+int main()
+{
+int num = 9;
+for(;num>0;num--)
+{
+for(int itr =9;itr >num;itr--)
+{
+        printf("*");
+}
+printf("\n");
+}
+printf("\n");
+printf("\n");
+printf("\n");
+printf("\n");
+printf("\n");
+
+int axe=0;
+for(;axe<9;axe++)
+{
+for(int itr=axe;itr<9;itr++)
+{
+	printf("*");
+}
+printf("\n");
+}
+printf("\n");
+printf("\n");
+printf("\n");
+printf("\n");
+printf("\n");
+
+int pi = 5;
+for(;pi>0;pi--)
+{
+for(int itr =5;itr >pi;itr--)
+{
+        printf("*");
+}
+printf("\n");
+}
+int pic=0;
+for(;pic<5;pic++)
+{
+for(int itr=pic;itr<5;itr++)
+{
+	printf("*");
+}
+printf("\n");
+}
+int alpha =5;
+while (alpha)
+{
+printf("its running ");
+alpha--;
+}
+int a=5;
+do{
+
+printf("hello");
+a--;
+}
+while(a);
+}
+
+
